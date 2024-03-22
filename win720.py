910c110368944cfdbe2126583716c53a227f6846
bangwook <wook2yam@gmail.com>
2024-03-22 19:43:14 +0900

    Update win720.py

--- win720.py
+++ win720.py
@@ -50,12 +50,29 @@ class Win720:
 
         headers = self._generate_req_headers(auth_ctrl)
 
+        print(f"headers : {headers}")
+
+
         self.keyCode = headers['Cookie'].split("JSESSIONID=")[1]
+
+        print(f"self.keyCode : {self.keyCode}")
+        
         win720_round = self._get_round()
+
+        print(f"win720_round : {win720_round}")
         
         makeAutoNum_ret = self._makeAutoNumbers(auth_ctrl, win720_round)
+
+        print(f"makeAutoNum_ret : {makeAutoNum_ret}")
+        
         parsed_ret = self._decText(json.loads(makeAutoNum_ret)['q']) 
+
+        print(f"parsed_ret : {parsed_ret}")
+        
         extracted_num = json.loads(parsed_ret)["selLotNo"]
+
+        print(f"extracted_num : {extracted_num}")
+        
         orderNo, orderDate = self._doOrderRequest(auth_ctrl, win720_round, extracted_num)
         
         body = json.loads(self._doConnPro(auth_ctrl, win720_round, extracted_num, orderNo, orderDate))

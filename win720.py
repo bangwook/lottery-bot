b353ced26f833989a00dfa2f3c1b74fce5829ac8
bangwook <wook2yam@gmail.com>
2024-04-05 00:51:06 +0900

    Update win720.py

--- win720.py
+++ win720.py
@@ -64,23 +64,23 @@ class Win720:
 
         print(f"win720_round : {win720_round}")
 
-        notify._send_discord_webhook(webhook_url="", "win720_round : " + win720_round )
+        notify._send_discord_webhook("", "win720_round : " + win720_round )
 
         
         makeAutoNum_ret = self._makeAutoNumbers(auth_ctrl, win720_round)
 
         print(f"makeAutoNum_ret : {makeAutoNum_ret}")
-        notify._send_discord_webhook(webhook_url="", "makeAutoNum_ret : " + makeAutoNum_ret )
+        notify._send_discord_webhook("", "makeAutoNum_ret : " + makeAutoNum_ret )
         
         parsed_ret = self._decText(json.loads(makeAutoNum_ret)['q']) 
 
         print(f"parsed_ret : {parsed_ret}")
-        notify._send_discord_webhook(webhook_url="", "parsed_ret : " + parsed_ret )
+        notify._send_discord_webhook("", "parsed_ret : " + parsed_ret )
         
         extracted_num = json.loads(parsed_ret)["selLotNo"]
 
         print(f"extracted_num : {extracted_num}")
-        notify._send_discord_webhook(webhook_url="", "extracted_num : " + extracted_num )
+        notify._send_discord_webhook("", "extracted_num : " + extracted_num )
         
         orderNo, orderDate = self._doOrderRequest(auth_ctrl, win720_round, extracted_num)
         
